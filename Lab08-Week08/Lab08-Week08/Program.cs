7704ec844f0fab8280507083c85c121c0a952d9c
Surasit <surasitboss229@gmail.com>
2017-10-15 18:08:54 +0700

    W08P05

--- Lab08-Week08/Lab08-Week08/Program.cs
+++ Lab08-Week08/Lab08-Week08/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Lab08_Week08
+namespace ConsoleAppArray
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int[] data = { 51, 58, 14, 20, -5, 61, 7, 21, 6 };
+            Console.WriteLine("Length of data = {0}", data.Length);
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine(data[i]);
+            }
+            Console.ReadLine();
         }
     }
 }
