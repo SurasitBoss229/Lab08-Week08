47e453083e8445fd989da9052c7cbf7413b77614
Surasit <surasitboss229@gmail.com>
2017-10-15 18:04:26 +0700

    W08P03

--- Lab08-Week08/Lab08-Week08/Program.cs
+++ Lab08-Week08/Lab08-Week08/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Lab08_Week08
+namespace ConsoleAppArray
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int[] data = new int[10];
+            data[0] = 0; data[1] = 1; data[2] = 2; data[3] = 3; data[4] = 4;
+            data[5] = 5; data[6] = 6; data[7] = 7; data[8] = 8; data[9] = 9;
+            Console.WriteLine(data[7]);
+            Console.WriteLine(data[4] + data[9]);
+            if (data[2] == data[6])
+            {
+                Console.WriteLine("data[2] = data[6]");
+            }
+            else
+            {
+                Console.WriteLine("data[2] != data[6]");
+            }
+            Console.WriteLine("Sum : {0}", data[0] + data[1] + data[2] + data[3]
+                + data[4] + data[5] + data[6] + data[7] + data[8] + data[9]);
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.Write(data[i] + " ");
+            }
+            Console.ReadLine();
         }
     }
 }
