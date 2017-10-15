e6e86330e31c17e37c581bb1c5ea97305a0d4001
Surasit <surasitboss229@gmail.com>
2017-10-15 19:30:10 +0700

    W08P17

--- Lab08-Week08/Lab08-Week08/Program.cs
+++ Lab08-Week08/Lab08-Week08/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
-namespace Lab08_Week08
+namespace ArrayListDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ArrayList list = new ArrayList();
+            Console.WriteLine("Create a list");
+            Console.WriteLine("List capacity = {0,2}", list.Capacity);
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("Add int to list : {0,2} => ", i);
+                list.Add(i);
+                Console.WriteLine("List count =  {0,3}, capacity = {1,3}  ",
+    list.Count, list.Capacity);
+            }
+            Console.WriteLine("-----------------------");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("Remove int from list : {0,2} => ", i);
+                list.Remove(i);
+                Console.WriteLine("List count =  {0,3}, capacity = {1,3}  ",
+    list.Count, list.Capacity);
+            }
+            Console.ReadLine();
         }
     }
 }
